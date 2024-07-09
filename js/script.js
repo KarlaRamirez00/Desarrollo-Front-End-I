fcf654d24c275f168dd70785f5c4078def04f7f9
KarlaRamirez00 <karl.ramirez@duocuc.cl>
2024-07-09 12:47:58 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+
+//Alerta carrito
+function alerta(){
+    alert("Your cart is empty")
+}
+
+//Cambio de imagen
+function cambia_imagen(elemento_img){
+    elemento_img.src = "img/assets/succulents-2.jpg"
+}
+
+function regresa_imagen(elemento_img){
+    elemento_img.src = "img/assets/succulents-1.jpg"
+}
+
+//Eliminar cookies
+function eliminar_cookies() {
+    document.querySelector("footer").remove();
+}
+
+
+
+
+
+
+
+
+
